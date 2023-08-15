c8b7ef12c1e4fc3de28423e6aea340f1583c6e3b
Milena Grygier <milenagrygier@protonmail.com>
2023-08-15 21:31:05 +0200

    route

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Money\MoneyPackage\Controllers\MoneyController;
 
-Route::get('test', [MoneyController::class, 'show']);
+Route::get('/test', [MoneyController::class, 'show']);
