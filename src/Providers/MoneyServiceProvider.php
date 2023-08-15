cb6ac8f6e0f97787e7fd3d94ef46e9d6df639bff
Milena Grygier <milenagrygier@protonmail.com>
2023-08-15 19:58:24 +0200

    initial
    initial

---
+++ src/Providers/MoneyServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Money\MoneyPackage;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Support\DeferrableProvider;
+
+class MoneyServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+
+}
